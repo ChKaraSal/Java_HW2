395436211b5c84538efa7e909d0d467f6805be24
chayan <chayan.karasal@internet.ru>
2023-07-02 09:00:38 +0700

    HW2

---
+++ Task_02.java
@@ -0,0 +1,35 @@
+// Используйте StringBuilder для подготовки ответа. Далее создайте метод, который запишет
+// результат работы в файл. Обработайте исключения и запишите ошибки в лог файл с помощью Logger.
+
+
+import java.io.IOException;
+import java.util.logging.*;
+public class Task_02 {
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger(Task_02.class.getName());
+        FileHandler fh = new FileHandler("logTask_02.xml");
+        logger.addHandler(fh);
+        XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(xml);
+        logger.info("Sort Array");
+        int [] array = {2, 5, 1, -7, 3, 64, 0};
+        lib.printArrayInt(array);
+        int[] newArr = listSort(array);
+        System.out.printf("\n Отсортированный массив: \n");
+        lib.printArrayInt(newArr);
+    }
+    public static int[] listSort(int [] arr){
+        for(int k = 0; k < arr.length-1; k++) {
+            
+            for (int i = 0; i < arr.length - k-1; i++){
+                if (arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    
+}
